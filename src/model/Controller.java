7230b03a16b8e43e1edb7c09ae98eced36250ea3
isaitotq <isaitotq@gmail.com>
2025-04-12 20:23:48 -0500

    quiubo profe tqm<3

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -6,9 +6,9 @@ import model.MatrizNumerica;
 
 public class Controller {
 
-    private SalaCine cinemark; // Declarando sala de cine
-    private MatrizNumerica matriz1;
-    private MatrizNumerica matriz2;
+    private SalaCine CineColombia; // Declarando sala de cine
+    private MatrizNumerica matriz1; // Declarando matriz numerica numero 1
+    private MatrizNumerica matriz2; // Declarando matriz numerica numero 2
 
     /**
      * Constructor de la clase Controller para inicializar 
@@ -17,39 +17,44 @@ public class Controller {
      * @post Se crea una instancia de Controller 
      */
     public Controller() {
-        matriz1 = new MatrizNumerica(2,2);
-        matriz2 = new MatrizNumerica(2,2);
+    }
+
+    public void crearMatrices(int filas, int columnas) {
+        matriz1 = new MatrizNumerica(filas, columnas);
+        matriz2 = new MatrizNumerica(filas, columnas);
+    }
+
+    public MatrizNumerica sumarMatrices() {
+        return matriz1.sumar(matriz2);
     }
 
     public void inicialiceSala() {
-        cinemark = new SalaCine(7,7);
+        CineColombia = new SalaCine(7,7);
     }
 
     public String mostrarSala() {
-        return cinemark.toString();
+        return CineColombia.toString();
     }
 
     public void comprarSilla(int fila, int asientoEnFila) {
-        cinemark.reservarAsiento(fila, asientoEnFila);
+        CineColombia.reservarAsiento(fila, asientoEnFila);
     }
 
     public int cantidadFilas() {
-        return cinemark.retornarCantFilas();
+        return CineColombia.retornarCantFilas();
     }
 
     public int cantidadColumnas() {
-        return cinemark.retornarCantColumnas();
+        return CineColombia.retornarCantColumnas();
     }
 
-    public MatrizNumerica getMatriz1() {
+    public MatrizNumerica getMatriz1(){
         return matriz1;
     }
 
-    public MatrizNumerica getMatriz2() {
+    public MatrizNumerica getMatriz2(){
         return matriz2;
     }
-
-
 
 
 }
